91cb87db563ee6f22ec59accb522249decdf3f37
Amar Shaik <amarshaik012@gmail.com>
2025-06-30 21:30:11 +0530

    🔧 Fix: Set kreuzwerker/docker as provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,6 @@ terraform {
       version = "~> 2.25.0"
     }
   }
-  required_version = ">= 1.3.0"
 }
 
 provider "docker" {
@@ -13,26 +12,17 @@ provider "docker" {
 }
 
 resource "docker_image" "complete_cicd_image" {
-  name = "complete-cicd:latest"
+  name         = "complete-cicd:latest"
+  keep_locally = false
 }
 
 resource "docker_container" "complete_cicd_container" {
   name  = "complete-cicd-v2"
   image = docker_image.complete_cicd_image.image_id
-
   ports {
     internal = 5050
     external = 5052
-    ip       = "0.0.0.0"
-    protocol = "tcp"
   }
-
-  must_run       = true
-  restart        = "no"
-  start          = true
   remove_volumes = true
-
-  lifecycle {
-    replace_triggered_by = [docker_image.complete_cicd_image]
-  }
+  restart        = "no"
 }
